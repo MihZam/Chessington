bb3133156f50215ac08eff592b60fe7453c685f5
Mihai Zamfirescu <Mihai.Zamfirescu@softwire.com>
2020-07-14 18:36:17 +0100

    Functionality: Pawns can attack diagonally

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -15,16 +15,21 @@ namespace Chessington.GameEngine.Pieces
             var movesList = new List<Square>();
             Square farSquare;
             Square square;
+            var diagonalSquares = new List<Square>();
             
             if (Player == Player.Black)
             {
                 farSquare = new Square(row + 2, col);
                 square = new Square(row + 1, col);
+                diagonalSquares.Add(new Square(row + 1, col - 1));
+                diagonalSquares.Add(new Square(row + 1, col + 1));
             }
             else
             {
                 farSquare = new Square(row - 2, col);
                 square = new Square(row - 1, col);
+                diagonalSquares.Add(new Square(row - 1, col - 1));
+                diagonalSquares.Add(new Square(row - 1, col + 1));
             }
 
             if (square.IsWithinBoard() && board.GetPiece(square) == null)
@@ -34,6 +39,14 @@ namespace Chessington.GameEngine.Pieces
                 if (!hasMoved && board.GetPiece(farSquare) == null)
                 {
                     movesList.Add(farSquare);
+                }
+            }
+            
+            foreach (var sq in diagonalSquares)
+            {
+                if (sq.IsWithinBoard() && board.GetPiece(sq) != null && board.GetPiece(sq).Player != this.Player)
+                {
+                    movesList.Add(sq);
                 }
             }
 
