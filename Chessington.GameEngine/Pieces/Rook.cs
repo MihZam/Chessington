372b310cfabe4f3ce0ef0eab6aac1d78e82f08fc
Mihai Zamfirescu <Mihai.Zamfirescu@softwire.com>
2020-07-14 14:18:16 +0100

    Functionality: Rooks can move laterally

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,27 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var col = board.FindPiece(this).Col;
+            var row = board.FindPiece(this).Row;
+            var movesList = new List<Square>();
+
+            for (var i = 0; i < 8; i++)
+            {
+                if (i != row)
+                {
+                    movesList.Add(new Square(i, col));
+                }
+            }
+
+            for (var j = 0; j < 8; j++)
+            {
+                if (j != col)
+                {
+                    movesList.Add(new Square(row, j));
+                }
+            }
+
+            return movesList;
         }
     }
 }
