c1d1204499b122fb4b66dc9628dff6a2691d05b6
Mihai Zamfirescu <Mihai.Zamfirescu@softwire.com>
2020-07-14 15:19:03 +0100

    Functionality: Knight moves

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,23 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var col = board.FindPiece(this).Col;
+            var row = board.FindPiece(this).Row;
+            var movesList = new List<Square>();
+
+            movesList.Add(new Square(row-2, col-1));
+            movesList.Add(new Square(row-2, col+1));
+            
+            movesList.Add(new Square(row+2, col-1));
+            movesList.Add(new Square(row+2, col+1));
+            
+            movesList.Add(new Square(row-1, col-2));
+            movesList.Add(new Square(row+1, col-2));
+            
+            movesList.Add(new Square(row-1, col+2));
+            movesList.Add(new Square(row+1, col+2));
+            
+            return movesList;
         }
     }
 }
